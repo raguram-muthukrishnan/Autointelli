79b163ab3128c0e7d6d56189d0360d68174e55a0
Surajkumaar <m.surajkumaar13022005@gmail.com>
2025-12-19 00:29:11 +0530

    d14

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -17,6 +17,4 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
-  url: env('ADMIN_URL', '/strapi-admin'),
-  serveAdminPanel: env.bool('SERVE_ADMIN', true),
 });
