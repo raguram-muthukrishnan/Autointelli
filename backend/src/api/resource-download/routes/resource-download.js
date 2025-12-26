8b88971de3a054f4fdaa2618346a5723daf1face
Surajkumaar <m.surajkumaar13022005@gmail.com>
2025-12-26 20:27:01 +0530

    42

--- backend/src/api/resource-download/routes/resource-download.js
+++ backend/src/api/resource-download/routes/resource-download.js
@@ -6,4 +6,53 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::resource-download.resource-download');
+module.exports = {
+  routes: [
+    {
+      method: 'POST',
+      path: '/resource-downloads',
+      handler: 'resource-download.create',
+      config: {
+        policies: [],
+        middlewares: [],
+        auth: false, // Allow public access for POST
+      },
+    },
+    {
+      method: 'GET',
+      path: '/resource-downloads',
+      handler: 'resource-download.find',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'GET',
+      path: '/resource-downloads/:id',
+      handler: 'resource-download.findOne',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'PUT',
+      path: '/resource-downloads/:id',
+      handler: 'resource-download.update',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/resource-downloads/:id',
+      handler: 'resource-download.delete',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
